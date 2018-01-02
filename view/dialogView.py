a489b16526373197aee7afd39bf0c4ce2ffac103
michaelrkost <michaelrkost@outlook.com>
2018-01-01 20:43:00 -0500

    Running Up through Close Application

--- view/dialogView.py
+++ view/dialogView.py
@@ -3,56 +3,59 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QAction, QMessageBox
 
 
-from view.dialog import Ui_exchange
+from view.mainwindow import Ui_MainWindow
 
 
-def setup_window (ui_ex, Qwidget):
+def close_application():
+
+    print("hello")
+    # choice = QMessageBox.question(QWidget, 'Message',
+    #                               "Are you sure to quit?", QMessageBox.Yes |
+    #                               QMessageBox.No, QMessageBox.No)
+    #
+    # if choice == QMessageBox.Yes:
+    #     print('quit application')
+    #     sys.exit()
+    # else:
+    #     pass
+
+def setup_window (ui_main_window, an_app):
     # update the Drop Down menus
     security_types = ["STK", "IND", "OPT", "FUT", "FOP", "CASH", "BAG", "NEWS"]
-    ui_ex.securityType.addItems(security_types)
+    ui_main_window.securityType.addItems(security_types)
     exchange_types = ["SMART", "CBOE", "AMEX", "IDEAL", "ISLAND", "NYSE", "PHLX"]
-    ui_ex.exchangeLoc.addItems(exchange_types)
+    ui_main_window.exchangeLoc.addItems(exchange_types)
     callPut_types = ["Call", "Put"]
-    ui_ex.callPut.addItems(callPut_types)
+    ui_main_window.callPut.addItems(callPut_types)
     currency_types = ["USD", "EUR", "JPY", "GBP", "INR", "CAD", "AUD"]
-    ui_ex.currency.addItems(currency_types)
+    ui_main_window.currency.addItems(currency_types)
+
+    print("an_app:         ", an_app)
+    print("ui_main_window: ", ui_main_window)
+    print("ui_main_window.actionQuit:        ", ui_main_window.actionQuit)
+
+    ui_main_window.actionQuit.triggered.connect(close_application)
 
     # add the IB icon
-    Qwidget.setWindowIcon(QIcon('icons\ib.png'))
-
-    # # Set up application exit
-    exit_app = QAction('&Exit', Qwidget)
-    exit_app.setShortcut('Ctrl+Q')
-    exit_app.setStatusTip('leave the app')
-    print("exit_app:  ", exit_app)
-    #exit_app.triggered.connect(sys.exit())
-    #
-    # # create and add Menu
-    mainMenu = QMainWindow.menuBar()
-    fileMenu = mainMenu.addMenu('&File')
-    fileMenu.addAction(exit_app)
-    #
-    # QMainWindow.statusBar()
+    QWidget.setWindowIcon(an_app, QIcon('icons\ib.png'))
 
 
 if __name__ == "__main__":
-    # create QT window
+    # create main window
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    exchange = QtWidgets.QDialog()
-    ui = Ui_exchange()
-    ui.setupUi(exchange)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
 
-    print("QtWidgets: ", QtWidgets)
-    print("app:       ", app)
-    print("exchange:  ", exchange)
-    print("ui:        ", ui)
+    setup_window(ui, MainWindow)
 
-    # setup details of QT window
-    setup_window(ui, app)
+    MainWindow.show()
 
-    exchange.show()
+    print("app:        ", app)
+    print("MainWindow: ", MainWindow)
+    print("ui:         ", ui)
+
     sys.exit(app.exec_())
-
 
 
